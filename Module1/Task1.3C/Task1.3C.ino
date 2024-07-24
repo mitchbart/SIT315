d425962b0def6c32ab55a542f06259007c88d0f2
mitchieb <mitchbart@proton.me>
2024-07-24 18:40:46 +1000

    finished 3c

--- Module1/Task1.3C/Task1.3C.ino
+++ Module1/Task1.3C/Task1.3C.ino
@@ -2,12 +2,7 @@
 const uint8_t RED_LED_PIN = 13;
 const uint8_t BLUE_LED_PIN = 12;
 
-// photo tran values
-//int value = 0;
-//int photoTran = A0;
-int photoTran = 5;
-
-// Init tiltState - will change
+// Init volatile states - will change
 volatile bool tiltState = false;
 volatile bool lightState = false;
 
@@ -15,37 +10,18 @@ volatile bool lightState = false;
 void setup() {
   DDRB |= 0b00110000;  // assign led pins 12 and 13 to output;
   PCICR |= 0b00000100;  // enable pin change interrupt for Port D
-  //PCMSK2 |= 0b00010000;  // enable interrupt for pin 4
   PCMSK2 |= 0b00110000;  // enable interrupt for pins 4 and 5
   Serial.begin(9600);  //establish serial connection with standard baud rate
 }
 
-// Loop left in so we can see it be interrupted
 void loop() {
-  //Serial.print("Loop begin");
-  // value = digitalRead(photoTran);
-  // //value = analogRead(photoTran);
-  // Serial.println(value);
-  // if (value > 0) {
-  //   tone(8, 262);
-  // }
-  // else {
-  //   noTone(8);
-  // }
-  // delay(1000);
-  // Serial.print(".");
-  // delay(1000);
-  // Serial.print(".");
-  // delay(1000);
-  // Serial.print(". ");
-  //delay(2000);
-  //Serial.println("Loop end.");
+  // Empty
 }
 
 // Interrupt service routine watching port D
 ISR(PCINT2_vect) {
-  Serial.println("");
   Serial.println("INTERRUPT!");
+  // Get and print tiltState
   tiltState = PIND & 0b00010000;  // byte code for digitalRead(4)
   Serial.print("tiltState: ");
   Serial.println(tiltState);  
@@ -54,18 +30,16 @@ ISR(PCINT2_vect) {
   digitalWrite(RED_LED_PIN, !tiltState);
   digitalWrite(BLUE_LED_PIN, tiltState);
 
-
+  // Get and print lightState
   lightState = PIND & 0b00100000;  // byte code for digitalRead(5)
   Serial.print("lightState: ");
   Serial.println(lightState);
-  if (lightState) {
-    tone(8, 262, 100);
+  
+  // Play a tone while light is detected 
+  if (lightState != 0) {
+    tone(8, 42);
   }
-  // if (lightState == 1) {
-  //   tone(8, 262);
-  // }
-  // else {
-  //   noTone(8);
-  // }
-
+  else {
+    noTone(8);
+  }
 }
